1036bcf469d545f9cf12bb51cfb9c8fb2bd69941
developerdatascience <harendra091016@gmail.com>
2022-07-29 16:05:07 +0530

    Adding src changes

--- src/train.py
+++ src/train.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
@@ -25,4 +26,22 @@ y = heart_data['target']
 
 
 #Splitting the data into training and test data
-X_train, X_test, Y_train, Y_test = train_test_split()
+X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=3)
+
+
+#Model Training :- Logistic Regression
+
+#Training the Logistic regression model with training data
+model = LogisticRegression()
+
+model.fit(X_train, Y_train)
+
+X_train_prediction = model.predict(X_train)
+train_accuracy_score = accuracy_score(X_train_prediction,Y_train)
+
+print("Accuracy score on training data: ", train_accuracy_score)
+
+#Testing the Logistic Regression model with test data
+X_test_prediction = model.predict(X_test)
+test_accuracy_score = accuracy_score(X_test_prediction, Y_test)
+
