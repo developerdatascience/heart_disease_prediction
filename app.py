677e9f914558b9132db1dcbc38a216960688b27e
developerdatascience <harendra091016@gmail.com>
2022-07-30 13:45:43 +0530

    Server files added

---
+++ app.py
@@ -0,0 +1,41 @@
+import json
+import pickle
+
+from flask import Flask, request, app, jsonify, url_for, render_template
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+#Load the model
+logmodel = pickle.load(open("logisticmodel.pkl", "rb"))
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data =np.array(list(data.values())).reshape(1,-1)
+    output = logmodel.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input=np.array(data).reshape(1, -1)
+    print(final_input)
+    output = logmodel.predict(final_input)[0]
+    if output == 0:
+        return render_template("home.html", prediction_text="Person does not have heart disease")
+    else:
+        render_template("home.html", prediction_text="Person have heart disease")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
